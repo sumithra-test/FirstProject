07eb949f8225d9aefb39de22fb310b5068f88d6c
sumithra-test <72287131+sumithra-test@users.noreply.github.com>
2020-10-02 21:23:51 -0500

    Add files via upload

---
+++ TestInput.java
@@ -0,0 +1,17 @@
+package Test;
+
+import java.util.Scanner;
+
+public class TestInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner inputName = new Scanner(System.in);
+		
+		String name = inputName.next();
+		
+		System.out.println(" welcome to java selenium " + name);
+	}
+
+}
